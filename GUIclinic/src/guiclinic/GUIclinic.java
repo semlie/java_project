2379918944c969a47a910300028f19182bb03a6e
unknown <israellieb@gmail.com>
2014-04-03 16:49:50 +0300

    add GUI project

--- GUIclinic/src/guiclinic/GUIclinic.java
+++ GUIclinic/src/guiclinic/GUIclinic.java
@@ -6,17 +6,25 @@
 
 package guiclinic;
 
+import DataAccessObject.Backend_DAO_List_impl;
+import com.github.javafaker.Faker;
+
 /**
  *
  * @author Dell
  */
 public class GUIclinic {
-
+public static  Backend_DAO_List_impl Controler;
+static {
+Controler = new Backend_DAO_List_impl();
+}
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+   Faker f = new Faker();
+        System.out.println(f.name());
     }
     
 }
