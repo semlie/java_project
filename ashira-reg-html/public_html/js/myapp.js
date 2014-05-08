9d7bf1711be4687cebea1a75a0449f051bef4bb9
Admin <Admin@Admin-PC>
2014-05-08 21:43:25 +0300

    build the form

--- ashira-reg-html/public_html/js/myapp.js
+++ ashira-reg-html/public_html/js/myapp.js
@@ -8,7 +8,8 @@
 var myApp = angular.module('regform', [
     'myApp.directives',
     'myApp.controllers',
-    'ngRoute'
+    'ngRoute',
+    'ui.bootstrap'
 ]);
 
 
@@ -28,25 +29,80 @@ angular.module('myApp.controllers', []).
 //                    });
         $scope.fildes = [{
                 'name': "email",
-                "label":"כתובת מייל",
                 "type": "email",
                 "id": "email",
                 "errMsg": "אימייל לא תקין",
-                "placeholder":"כתובת המייל"
+                "label": "כתובת מייל",
+                "placeholder": "כתובת המייל"
             },
             {
                 'name': "password",
                 "type": "password",
                 "id": "password",
-                "errMsg": "בעיה בסיסמה"
+                "errMsg": "בעיה בסיסמה",
+                "label": "סיסמה לאתר אשירה",
+                "placeholder": "סיסמה שתשמש אותך בכניסתך לאתר אשירה"
             },
             {
                 'name': "user-name",
                 "type": "text",
                 "id": "username",
-                "errMsg": "שם משתמש כבר קיים"
+                "errMsg": "שם משתמש כבר קיים",
+                "label": "שם משתמש לאתר אשירה",
+                "placeholder": "שם המשתמש בכניסתך לאתר אשירה"
+            },
+            {
+                'name': "card_owner",
+                "type": "text",
+                "id": "card_owner",
+                "errMsg": "שם משתמש כבר קיים",
+                "label": "שם בעל הכרטיס",
+                "placeholder": "שם בעל הכרטיס כפי שמופיע על הכרטיס"
+            },
+            {
+                'name': "owner_phone",
+                "type": "text",
+                "id": "owner_phone",
+                "errMsg": "שם משתמש כבר קיים",
+                "label": "מספר טלפון של בעל הכרטיס",
+                "placeholder": "מספר טלפון כולל קידומת"
+            },
+            {
+                'name': "card_number",
+                "type": "text",
+                "id": "card_number",
+                "errMsg": "שם משתמש כבר קיים",
+                "label": "מספר כרטיס אשראי",
+                "placeholder": "מספר כרטיס אשראי כפי שמופיע על הכרטיס"
+            },
+            {
+                'name': "owner_email",
+                "type": "text",
+                "id": "owner_email",
+                "errMsg": "שם משתמש כבר קיים",
+                "label": "אי-מייל למשלוח חשבונית",
+                "placeholder": "אי-מייל למשלוח חשבונית"
             }
         ];
+        
+        $scope.user ={};
+        $scope.user.submitTheForm = function(item, event) {
+            console.log("--> Submitting form");
+            var dataObject = {
+                name: $scope.myForm.name
+                , car: $scope.myForm.car
+            };
+
+            var responsePromise = $http.post("/angularjs-examples/json-test-data.jsp", dataObject, {});
+            responsePromise.success(function(dataFromServer, status, headers, config) {
+                console.log(dataFromServer.title);
+            });
+            responsePromise.error(function(data, status, headers, config) {
+                alert("Submitting form failed!");
+            });
+        };
+
+
     }]);
 
 
@@ -60,15 +116,22 @@ angular.module('myApp.directives', []).
                 restrict: 'E',
                 templateUrl: 'partial/menu.html'
             };
-        }).directive('carousel', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'partial/Carousel.html'
-    };
-}).directive('formReg', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'partial/form.html'
-    };
-});
-  
+        }).
+        directive('carousel', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'partial/Carousel.html'
+            };
+        }).
+        directive('formReg', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'partial/form.html'
+            };
+        }).
+        directive('inputs', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'partial/input-loop.html'
+            };
+        });
