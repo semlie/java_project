52b999c5f9d88400267b9ae333f43b61cc241055
Admin <Admin@Admin-PC>
2014-06-06 14:28:29 +0300

    after using DB and webService and client server with web client

--- BackedDAOAdapters/src/backeddaoadapters/BackendWSAdapter.java
+++ BackedDAOAdapters/src/backeddaoadapters/BackendWSAdapter.java
@@ -15,7 +15,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Locale;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import webservice.Exception_Exception;
 
 /**
@@ -24,13 +26,26 @@ import webservice.Exception_Exception;
  */
 public class BackendWSAdapter implements Backend {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
     }
 
-    public webservice.Counseling ConvertCounseling(TreatmentD.Counseling c) {
+    /**
+     *
+     * @param c
+     * @return
+     * @throws DatatypeConfigurationException
+     * @throws ParseException
+     */
+    public webservice.Counseling ConvertCounseling(TreatmentD.Counseling c) throws DatatypeConfigurationException, ParseException {
         webservice.Counseling _c = new webservice.Counseling();
         _c.setId(c.getId());
+//        XMLGregorianCalendarImpl d=new XMLGregorianCalendarImpl();
+//        d.toGregorianCalendar().setTime(c.getDate());
         _c.setDate(convDate(c.getDate()));
         _c.setNameOfAttendant(c.getNameOfAttendant());
         _c.setPrice(c.getPrice());
@@ -38,11 +53,29 @@ public class BackendWSAdapter implements Backend {
         return _c;
     }
 
-    public XMLGregorianCalendarImpl convDate(Date d) {
-        GregorianCalendar calendar = new GregorianCalendar();
-        calendar.setTime(d);
-        return new XMLGregorianCalendarImpl(calendar);
+    /**
+     *
+     * @param d
+     * @return
+     * @throws DatatypeConfigurationException
+     */
+    public XMLGregorianCalendar convDate(Date d) throws DatatypeConfigurationException {
+        GregorianCalendar c = new GregorianCalendar();
+        if (d == null) {
+            d = new Date();
+        }
+        c.setTime(d);
+        XMLGregorianCalendar da = new XMLGregorianCalendarImpl();
+        da = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+        return da;
     }
+//   from the auto genertor
+//    public void setDate(Date value) throws DatatypeConfigurationException, ParseException {
+//        SimpleDateFormat ss = new SimpleDateFormat("yyyy-mm-dd");
+//        GregorianCalendar cal = new GregorianCalendar();
+//          cal.setTime(stringToJavaDate(ss.format(value)));
+//        this.date=DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+//    }
 
     /**
      *
@@ -52,14 +85,21 @@ public class BackendWSAdapter implements Backend {
     public TreatmentD.Counseling ConvertCounseling(webservice.Counseling c) throws ParseException {
         TreatmentD.Counseling _c = new TreatmentD.Counseling();
         _c.setId(c.getId());
-        _c.setDate(new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse(c.getDate().toString()));
+        _c.setDate(new SimpleDateFormat("yyyy-MM-dd").parse(c.getDate().toString()));
         _c.setNameOfAttendant(c.getNameOfAttendant());
         _c.setPrice(c.getPrice());
         _c.setTyps(TreatmentD.Counseling.TypesCons.valueOf(c.getTyps().name()));
         return _c;
     }
 
-    public webservice.Treatment ConvertTreatment(TreatmentD.Treatment t) {
+    /**
+     *
+     * @param t
+     * @return
+     * @throws DatatypeConfigurationException
+     * @throws ParseException
+     */
+    public webservice.Treatment ConvertTreatment(TreatmentD.Treatment t) throws DatatypeConfigurationException, ParseException {
         webservice.Treatment _t = new webservice.Treatment();
         _t.setDate(convDate(t.getDate()));
         _t.setId(t.getId());
@@ -70,9 +110,15 @@ public class BackendWSAdapter implements Backend {
         return _t;
     }
 
+    /**
+     *
+     * @param t
+     * @return
+     * @throws ParseException
+     */
     public TreatmentD.Treatment ConvertTreatment(webservice.Treatment t) throws ParseException {
         TreatmentD.Treatment _t = new TreatmentD.Treatment();
-        _t.setDate(new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse(t.getDate().toString()));
+        _t.setDate(new SimpleDateFormat("MMMM d, yyyy").parse(t.getDate().toString()));
         _t.setId(t.getId());
         _t.setNameOfAttendant(t.getNameOfAttendant());
         _t.setPrice(t.getPrice());
@@ -80,8 +126,33 @@ public class BackendWSAdapter implements Backend {
         _t.setTypes(TreatmentD.Services.TypesTret.valueOf(t.getTypes().name()));
         return _t;
     }
+//
+//    public webservice.Services ConvertService(TreatmentD.Services s) throws DatatypeConfigurationException, ParseException {
+//        webservice.Services _s = new webservice.Services();
+//        _s.setDate(s.getDate());
+//        _s.setId(s.getId());
+//        _s.setNameOfAttendant(s.getNameOfAttendant());
+//        _s.setPrice(s.getPrice());
+//        return _s;
+//    }
+//
+//    public TreatmentD.Services ConvertService(webservice.Services s) throws ParseException {
+//        TreatmentD.Services _s = new TreatmentD.Services();
+//        _s.setId(s.getId());
+//        _s.setNameOfAttendant(s.getNameOfAttendant());
+//        _s.setPrice(s.getPrice());
+//        _s.setDate(s.getDate().toGregorianCalendar().getTime());
+//        return _s;
+//    }
 
-    public webservice.Services ConvertService(TreatmentD.Services s) {
+    /**
+     *
+     * @param s
+     * @return
+     * @throws DatatypeConfigurationException
+     * @throws ParseException
+     */
+    public webservice.Services ConvertService(TreatmentD.Services s) throws DatatypeConfigurationException, ParseException {
         webservice.Services _s;
         if (s instanceof TreatmentD.Counseling) {
             _s = new webservice.Counseling();
@@ -89,56 +160,98 @@ public class BackendWSAdapter implements Backend {
             _s.setId(s.getId());
             _s.setNameOfAttendant(s.getNameOfAttendant());
             _s.setPrice(s.getPrice());
-
+            ((webservice.Counseling) _s).setTyps(webservice.TypesCons.valueOf(((TreatmentD.Counseling) s).getTyps().toString()));
             return _s;
-        } else {
+        } else if (s instanceof TreatmentD.Treatment) {
             webservice.Treatment _t = new webservice.Treatment();
-            TreatmentD.Treatment _ss = new TreatmentD.Treatment((TreatmentD.Treatment) s);
-
             _t.setDate(convDate(s.getDate()));
             _t.setId(s.getId());
             _t.setNameOfAttendant(s.getNameOfAttendant());
             _t.setPrice(s.getPrice());
-            _t.setProblem(_ss.getProblem());
+            _t.setProblem(((TreatmentD.Treatment) s).getProblem());
+            ((webservice.Treatment) _t).setTypes(webservice.TypesTret.valueOf(((TreatmentD.Treatment) s).getTypes().toString()));
 
             return _t;
         }
-
+        return null;
     }
 
+    /**
+     *
+     * @param s
+     * @return
+     * @throws ParseException
+     */
     public TreatmentD.Services ConvertService(webservice.Services s) throws ParseException {
         TreatmentD.Services _s;
         if (s instanceof webservice.Counseling) {
-            _s = new TreatmentD.Counseling(ConvertCounseling((webservice.Counseling) s));
-        } else {
-            _s = new TreatmentD.Treatment(ConvertTreatment((webservice.Treatment) s));
+            _s = new TreatmentD.Counseling();
+            _s.setDate(s.getDate().toGregorianCalendar().getTime());
+            _s.setId(s.getId());
+            _s.setNameOfAttendant(s.getNameOfAttendant());
+            _s.setPrice(s.getPrice());
+            ((TreatmentD.Counseling) _s).setTyps(TreatmentD.Counseling.TypesCons.valueOf(((webservice.Counseling) s).getTyps().name()));
 
+            return _s;
+        } else if (s instanceof webservice.Treatment) {
+            TreatmentD.Treatment _t = new TreatmentD.Treatment();
+            webservice.Treatment _ss = (webservice.Treatment) s;
+            _t.setDate(s.getDate().toGregorianCalendar().getTime());
+            _t.setId(s.getId());            _t.setNameOfAttendant(s.getNameOfAttendant());
+            _t.setPrice(s.getPrice());
+            _t.setProblem(_ss.getProblem());
+            ((TreatmentD.Treatment) _t).setTypes(TreatmentD.Treatment.TypesTret.valueOf(((webservice.Treatment) s).getTypes().name()));
+
+            return _t;
         }
-
-        return _s;
+        return null;
 
     }
 
-    public webservice.Meeting convertMeeting(TreatmentD.Meeting m) {
+    /**
+     *
+     * @param m
+     * @return
+     * @throws DatatypeConfigurationException
+     * @throws ParseException
+     */
+    public webservice.Meeting convertMeeting(TreatmentD.Meeting m) throws DatatypeConfigurationException, ParseException {
         webservice.Meeting _m = new webservice.Meeting();
         _m.setId(m.getId());
         _m.setPatient(ConvertPatient(m.getPatient()));
         _m.setPayed(m.isPayed());
-        _m.setPayed(m.isPayed());
+        
+        if (m.getDate()!=null) {
+            _m.setDate(convDate(m.getDate()));
+        }
         _m.setService(ConvertService(m.getService()));
+
         return _m;
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     * @throws ParseException
+     */
     public TreatmentD.Meeting convertMeeting(webservice.Meeting m) throws ParseException {
         TreatmentD.Meeting _m = new TreatmentD.Meeting();
         _m.setId(m.getId());
         _m.setPatient(ConvertPatient(m.getPatient()));
         _m.setPayed(m.isPayed());
-        _m.setPayed(m.isPayed());
+        if (m.getDate()!=null) {
+            _m.setDate(m.getDate().toGregorianCalendar().getTime());
+        }
         _m.setService(ConvertService(m.getService()));
         return _m;
     }
 
+    /**
+     *
+     * @param p
+     * @return
+     */
     public webservice.Patient ConvertPatient(TreatmentD.Patient p) {
         webservice.Patient p_WC = new webservice.Patient();
         p_WC.setId(p.getId());
@@ -147,6 +260,11 @@ public class BackendWSAdapter implements Backend {
         return p_WC;
     }
 
+    /**
+     *
+     * @param p
+     * @return
+     */
     public TreatmentD.Patient ConvertPatient(webservice.Patient p) {
         TreatmentD.Patient p_WC = new TreatmentD.Patient();
         p_WC.setId(p.getId());
@@ -269,6 +387,11 @@ public class BackendWSAdapter implements Backend {
         return port.treatmentType();
     }
 
+    /**
+     *
+     * @param PL
+     * @throws Exception
+     */
     @Override
     public void changePatient(ArrayList<Patient> PL) throws Exception {
         ArrayList<webservice.Patient> _PL = new ArrayList<>();
@@ -278,6 +401,11 @@ public class BackendWSAdapter implements Backend {
         this.changePatient(_PL);
     }
 
+    /**
+     *
+     * @param ML
+     * @throws Exception
+     */
     @Override
     public void changeMeeting(ArrayList<Meeting> ML) throws Exception {
         ArrayList<webservice.Meeting> _ML = new ArrayList<>();
@@ -287,6 +415,11 @@ public class BackendWSAdapter implements Backend {
         this.changeMeeting(_ML);
     }
 
+    /**
+     *
+     * @param SL
+     * @throws Exception
+     */
     @Override
     public void changeServices(ArrayList<Services> SL) throws Exception {
         ArrayList<webservice.Services> _SL = new ArrayList<>();
@@ -301,6 +434,11 @@ public class BackendWSAdapter implements Backend {
         this.addPatiant(ConvertPatient(p));
     }
 
+    /**
+     *
+     * @param p
+     * @throws Exception
+     */
     @Override
     public void DelPatient(Patient p) throws Exception {
         this.delPatient(ConvertPatient(p));
@@ -312,23 +450,43 @@ public class BackendWSAdapter implements Backend {
         this.addMeeting(convertMeeting(m));
     }
 
+    /**
+     *
+     * @param m
+     * @throws Exception
+     */
     @Override
     public void DelMeeting(Meeting m) throws Exception {
         this.delMeeting(convertMeeting(m));
 
     }
 
+    /**
+     *
+     * @param s
+     * @throws Exception
+     */
     @Override
     public void AddService(Services s) throws Exception {
         this.addService(ConvertService(s));
     }
 
+    /**
+     *
+     * @param s
+     * @throws Exception
+     */
     @Override
     public void DelService(Services s) throws Exception {
         this.delService(ConvertService(s));
 
     }
 
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     @Override
     public ArrayList<Patient> GetAllPatient() throws Exception {
         ArrayList<Patient> _p = new ArrayList<>();
@@ -338,6 +496,11 @@ public class BackendWSAdapter implements Backend {
         return _p;
     }
 
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     @Override
     public ArrayList<Services> GetAllServices() throws Exception {
         ArrayList<Services> _s = new ArrayList<>();
@@ -347,6 +510,11 @@ public class BackendWSAdapter implements Backend {
         return _s;
     }
 
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     @Override
     public ArrayList<Meeting> GetAllMeetings() throws Exception {
         ArrayList<Meeting> _s = new ArrayList<>();
@@ -356,6 +524,12 @@ public class BackendWSAdapter implements Backend {
         return _s;
     }
 
+    /**
+     *
+     * @param p
+     * @return
+     * @throws Exception
+     */
     @Override
     public ArrayList<Meeting> GetPatientMeeting(Patient p) throws Exception {
         ArrayList<Meeting> _s = new ArrayList<>();
@@ -365,22 +539,46 @@ public class BackendWSAdapter implements Backend {
         return _s;
     }
 
+    /**
+     *
+     * @param p
+     * @return
+     * @throws Exception
+     */
     @Override
     public double CalcPatientUnpaidMeeting(Patient p) throws Exception {
         return this.calcPatientUnpaidMeeting(ConvertPatient(p));
     }
 
+    /**
+     *
+     * @param p
+     * @return
+     * @throws Exception
+     */
     @Override
     public double CalcPatientPaid(Patient p) throws Exception {
         return this.calcPatientPaid(ConvertPatient(p));
 
     }
 
+    /**
+     *
+     * @param p
+     * @return
+     * @throws Exception
+     */
     @Override
     public double CalcPatientTotalCost(Patient p) throws Exception {
         return this.calcPatientTotalCost(ConvertPatient(p));
     }
 
+    /**
+     *
+     * @param metting
+     * @return
+     * @throws Exception
+     */
     @Override
     public double CalcAllPatientTotalCost(ArrayList<Meeting> metting) throws Exception {
         ArrayList<webservice.Meeting> _m = new ArrayList<>();
@@ -391,11 +589,21 @@ public class BackendWSAdapter implements Backend {
 
     }
 
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     @Override
     public ArrayList<String> counselingType() throws Exception {
         return (ArrayList<String>) this.counselingType1();
     }
 
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
     @Override
     public ArrayList<String> treatmentType() throws Exception {
         return (ArrayList<String>) this.treatmentType1();
